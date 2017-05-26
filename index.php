8f4031418de8487f848cbb5972289684c220fd91
jedaiascorreia <jhednos@hotmail.com>
2017-05-26 18:42:20 -0300

    atualização 26-05-17

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 	header('Content-Type: application/json;charset=utf-8');
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: Content-Type");
+	
 	require 'Slim/Slim.php';
 	\Slim\Slim::registerAutoloader();
 	$app = new \Slim\Slim();
